c36b87b20a5de68884efb5211cc59fcaa99cddf2
Hailton Martins Junior <hailton.martins-terceiro@letsbank.com.br>
2022-08-26 18:26:57 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
