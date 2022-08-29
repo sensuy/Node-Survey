a85ae57b55731e760080d03065f312259d76f6df
Hailton Martins Junior <hailton.martins-terceiro@letsbank.com.br>
2022-08-29 15:54:25 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,15 +4,16 @@ import { badRequest } from './helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
     return {
-      statusCode: 400,
-      body: new MissingParamError('Missing param: email')
+      statusCode: 200,
+      body: 'Success'
     }
   }
 }
